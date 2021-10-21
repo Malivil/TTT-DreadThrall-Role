21bff754275262217602434e8b7e2f2453718630
Malivil <mlebson@outlook.com>
2021-10-20 22:08:52 -0400

    Added more skeleton to the role

--- lua/customroles/dreadthrall.lua
+++ lua/customroles/dreadthrall.lua
@@ -6,12 +6,17 @@ ROLE.nameplural = "Dread Thralls"
 ROLE.nameext = "a Dread Thrall"
 ROLE.nameshort = "thr"
 
-ROLE.desc = [[]]
+ROLE.desc = [[You are {role}! {comrades}
+
+Use your bone charm to aid your team
+in defeating your enemies.
+
+Press {menukey} to receive your special equipment!]]
 
 ROLE.team = ROLE_TEAM_TRAITOR
 
-ROLE.shop = nil
 ROLE.loadout = {"weapon_thr_bonecharm"}
+ROLE.startingcredits = 2
 
 ROLE.translations = {}
 
@@ -21,4 +26,13 @@ RegisterRole(ROLE)
 
 if SERVER then
     AddCSLuaFile()
+end
+
+if CLIENT then
+    hook.Add("TTTTutorialRoleText", "SummonerTutorialRoleText", function(role, titleLabel, roleIcon)
+        if role == ROLE_DREADTHRALL then
+            local roleColor = ROLE_COLORS[ROLE_TRAITOR]
+            return "The " .. ROLE_STRINGS[ROLE_DREADTHRALL] .. " is a member of the <span style='color: rgb(" .. roleColor.r .. ", " .. roleColor.g .. ", " .. roleColor.b .. ")'>traitor team</span> who can use their bone charm weapon to aid their team in defeating their enemies."
+        end
+    end)
 end
